9b6c3b349a404203648bb742e3ad0c6b7bf4224e
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2017-06-30 18:15:32 -0300

    add timeout

--- src/functions/database/index.ts
+++ src/functions/database/index.ts
@@ -1,4 +1,5 @@
 import * as Umzug from 'umzug'
+import * as Promise from 'bluebird'
 
 import { getDatabase } from '../../database'
 
@@ -13,24 +14,28 @@ const getMigrationsPath = () => {
 export const migrate = (event, context, callback) => {
   console.log('MIGRATION STARTED XXX19')
 
-  getDatabase().then((database) => {
-    const umzug = new Umzug({
-      storage: 'sequelize',
-      storageOptions: {
-        sequelize: database
-      },
-      migrations: {
-        params: [
-          database.getQueryInterface(),
-          database.constructor
-        ],
-        path: getMigrationsPath(),
-        pattern: /\.js$/
-      }
-    })
+  return Promise
+    .timeout(10000)
+    .then(() => {
+      return getDatabase().then((database) => {
+        const umzug = new Umzug({
+          storage: 'sequelize',
+          storageOptions: {
+            sequelize: database
+          },
+          migrations: {
+            params: [
+              database.getQueryInterface(),
+              database.constructor
+            ],
+            path: getMigrationsPath(),
+            pattern: /\.js$/
+          }
+        })
 
-    return umzug.up()
-      .then(() => callback(null))
-  })
-  .catch(err => console.log(err))
+        return umzug.up()
+        .then(() => callback(null))
+      })
+    })
+    .catch(err => console.log(err))
 }
