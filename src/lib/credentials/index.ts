ced7ac2b71cbd3cea2040b4f569fc835bdfd2764
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2017-07-01 11:21:55 -0300

    more logs and remove getEnv

--- src/lib/credentials/index.ts
+++ src/lib/credentials/index.ts
@@ -4,20 +4,24 @@ const Credstash = require('nodecredstash')
 
 export function getDatabasePassword () {
   console.log("dentro do getDatabasePassword")
-  const credstash = Credstash({
+  console.log(process.env)
+  const credstash = new Credstash({
     table: 'credential-store',
     awsOpts: { region: 'us-east-1' }
   })
 
-  if (getEnv() === 'test') {
-    return Promise.resolve('touchdown1!')
-  }
+  console.log("before getSecret")
 
   return credstash.getSecret({
     name: `${process.env.STAGE}/database/password`,
     version: 1,
     context: {}
   })
+    .then(result => {
+      console.log("after getSecret")
+      console.log(result)
+      return result
+    })
     .catch(err => {
       console.log("erro no credstash", err)
     })
